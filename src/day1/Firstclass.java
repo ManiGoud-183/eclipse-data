31d5ab7e764db97c10013105b6aaaf6e770297f9
Manigoud183 <133944667+Manigoud183@users.noreply.github.com>
2025-09-24 19:21:51 +0530

    modified with reshma good girl

--- src/day1/Firstclass.java
+++ src/day1/Firstclass.java
@@ -1,11 +1,14 @@
 package day1;
 
+
+
 public class Firstclass {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Hello India");
 		System.out.println("Hello Russia");
+		System.out.println("reshma is a good girl");
 	}
 	
 	
