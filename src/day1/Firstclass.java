94779f6ae34ac7e824c5a60aa3603beab51a8e87
Manigoud183 <133944667+Manigoud183@users.noreply.github.com>
2025-09-24 18:56:37 +0530

    we are pushing a project

---
+++ src/day1/Firstclass.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello India");
+		System.out.println("Hello Russia");
+	}
+	
+	
+
+}
