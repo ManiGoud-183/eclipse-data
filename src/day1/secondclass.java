07f02358e4ab3182d6d081da97d9652628a6a262
Manigoud183 <133944667+Manigoud183@users.noreply.github.com>
2025-09-24 19:17:33 +0530

    Adding Second file

---
+++ src/day1/secondclass.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class secondclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main method of second class");
+	}
+
+}
