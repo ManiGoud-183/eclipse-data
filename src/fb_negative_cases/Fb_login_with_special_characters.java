c4657d42c5a8a5ebfbaa54403677fc7a51602c09
Manigoud183 <133944667+Manigoud183@users.noreply.github.com>
2025-09-25 19:18:16 +0530

    Create a new branch for negative cases

---
+++ src/fb_negative_cases/Fb_login_with_special_characters.java
@@ -0,0 +1,13 @@
+package fb_negative_cases;
+
+public class Fb_login_with_special_characters {
+
+	public static void main(String[] args) {
+		System.out.println("User able to enter username as 1234##1234");
+		System.out.println("User able to enter password as +-_+-");
+		System.out.println("User able to click on login");
+		
+
+	}
+
+}
