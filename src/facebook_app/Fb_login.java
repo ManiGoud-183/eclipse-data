3ae15afd487094748c11b8f022b56d5cf854008a
Manigoud183 <133944667+Manigoud183@users.noreply.github.com>
2025-09-25 19:03:33 +0530

    pushing fb file

---
+++ src/facebook_app/Fb_login.java
@@ -0,0 +1,13 @@
+package facebook_app;
+
+public class Fb_login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Username:");
+		System.out.println("Password:");
+		System.out.println("Login");
+
+	}
+
+}
